5e2cef017ae159c5a907f9cb5ff42c544e9abf54
Camille Jackson <cj@mail.co.de>
2020-05-20 23:29:05 -0400

    get user and users

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 To run this project, compile the `main.go` file with `go build main.go`
 and then run the compiled executable with `./main`, or do both at once with `go run main.go`.
+
+TODO to verify app is working
+
+### Get a list of all users
+```
+curl http://localhost:8080/users
+```
+
+### Get a particular user
+```
+curl http://localhost:8080/users/Camille
+```
+
+### Delete a user
+```
+curl -XDELETE http://localhost:8080/users/Camille
+```
+
+### Create a user
+```
+curl -H 'content-type: application/json' -d '{"name": "Camille"}' http://localhost:8080/users
+```
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Name        string
+	Name        string `gorm:"unique"`
 	CreditCards []CreditCard
 }
 
@@ -36,29 +36,71 @@ func main() {
 	db.AutoMigrate(&CreditCard{})
 
 	// Create
+	err = db.Create(&User{Name: "Camille"}).Error
+	if err != nil {
+		fmt.Printf("couldn't create user: %v\n", err)
+	}
 	var user User
-	db.Create(&User{Name: "Camille"})
-	db.First(&user, 1)
+	db.Where(&User{Name: "Camille"}).First(&user)
 	db.Create(&CreditCard{Number: "4444333322221111", UserID: user.ID})
 	db.Create(&CreditCard{Number: "1010292938384747", UserID: user.ID})
 	db.Create(&CreditCard{Number: "1234567812345678", UserID: user.ID})
 
 	// db.First(&expense, "code = ?", "L1212") // find expense with code l1212
 
-	// Query
-	// db.First(&user, 1)
-
-
 	// // Update - update expense's price to 2000
 	// db.Model(&expense).Update("Price", 2000)
 
-	// Delete
-	// db.Where("Number", "9999101011111212").Delete(&CreditCard{})
-
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/users", ShowAllUsers)
+	router.HandleFunc("/users/{name}", ShowUser)
+	// router.HandleFunc("/users", Method: "post", CreateUser)
 	fmt.Println("Now listening at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// func CreateUser(Name string) {
+// 	db.FirstOrCreate(&user, User{Name: "non_existing"})
+
+// 	return 201 CREATED
+// }
+
+func ShowUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	params := mux.Vars(r)
+	name := params["name"]
+
+	var user User
+	db.Where(&User{Name: name}).First(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to get user. %v", err)
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func ShowAllUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var users User
+	db.Find(&users)
+
+	if err != nil {
+		log.Fatalf("Unable to get users. %v", err)
+	}
+
+	json.NewEncoder(w).Encode(users)
 }
 
 func Index(writer http.ResponseWriter, request *http.Request) {
@@ -70,10 +112,10 @@ func Index(writer http.ResponseWriter, request *http.Request) {
 
 	// Fetch Camille
 	var user User
-	db.First(&user, 1)
+	db.Preload("CreditCards").Where(&User{Name: "Camille"}).First(&user)
 	json.NewEncoder(writer).Encode(user)
-
-	var credit_cards []CreditCard
-	db.Model(&user).Related(&credit_cards)
-	json.NewEncoder(writer).Encode(credit_cards)
+	
+	// var credit_cards []CreditCard
+	// db.Model(&user).Related(&credit_cards)
+	// json.NewEncoder(writer).Encode(credit_cards)
 }
